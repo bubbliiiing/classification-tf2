ce621906b9d152b564f9a3d02c5cac8de6b85c8e
Bubbliiiing <3323290568@qq.com>
2022-04-19 18:21:54 +0800

    update lr

--- train.py
+++ train.py
@@ -16,9 +16,6 @@ from utils.dataloader import ClsDatasets
 from utils.utils import get_classes, get_lr_scheduler
 from utils.utils_fit import fit_one_epoch
 
-gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
 
 #----------------------------------------#
 #   主函数
