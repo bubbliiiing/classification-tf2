10704d8e1073364eaee76a9f60aed73ca7e7b554
Bubbliiiing <3323290568@qq.com>
2022-05-05 16:51:21 +0800

    update checkpoint

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -72,8 +72,8 @@ def fit_one_epoch(net, loss_history, optimizer, epoch, epoch_step, epoch_step_va
             batch_images, batch_labels = batch
 
             loss_value, acc = train_step(batch_images, batch_labels, net, optimizer)
-            total_loss  += loss_value
-            total_acc   += acc
+            total_loss  += loss_value.numpy()
+            total_acc   += acc.numpy()
 
             pbar.set_postfix(**{'total_loss'    : float(total_loss) / (iteration + 1), 
                                 'acc'           : float(total_acc) / (iteration + 1), 
@@ -89,17 +89,27 @@ def fit_one_epoch(net, loss_history, optimizer, epoch, epoch_step, epoch_step_va
             batch_images, batch_labels = batch
 
             loss_value, acc = val_step(batch_images, batch_labels, net, optimizer)
-            val_loss        += loss_value
-            val_acc         += acc
+            val_loss        += loss_value.numpy()
+            val_acc         += acc.numpy()
 
             pbar.set_postfix(**{'val_loss'  : float(val_loss) / (iteration + 1),
                                 'val_acc'   : float(val_acc) / (iteration + 1)})
             pbar.update(1)
     print('Finish Validation')
 
-    logs = {'loss': total_loss.numpy() / epoch_step, 'val_loss': val_loss.numpy() / epoch_step_val}
+    logs = {'loss': total_loss / epoch_step, 'val_loss': val_loss / epoch_step_val}
     loss_history.on_epoch_end([], logs)
     print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
     print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
+    
+    #-----------------------------------------------#
+    #   保存权值
+    #-----------------------------------------------#
     if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
-        net.save_weights(os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.h5" % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+        net.save_weights(os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.h5' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+        
+    if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
+        print('Save best model to best_epoch_weights.pth')
+        net.save_weights(os.path.join(save_dir, "best_epoch_weights.h5"))
+            
+    net.save_weights(os.path.join(save_dir, "last_epoch_weights.h5"))
